27df2937b7738c928ef73fa5f588fc85268dbb11
mvserikawa <mvserikawa@gmail.com>
2025-06-12 22:34:15 -0300

    fix(p2): finalmente funcionou a busca

--- src/components/Busca.jsx
+++ src/components/Busca.jsx
@@ -4,6 +4,8 @@ import {InputText} from 'primereact/inputtext'
 import {InputIcon} from 'primereact/inputicon'
 import axios from 'axios'
 import striptags from 'striptags'
+import Sanfona from './Sanfona'
+import PrevisaoLista from './PrevisaoLista'
 
 const Busca = () => {
     const [termoDeBusca, setTermoDeBusca] = useState('São Paulo') 
@@ -11,16 +13,25 @@ const Busca = () => {
 
     useEffect(() => {
         const fazerBusca = async () => {
-            const { data } = await axios.get('http://localhost:3000/search', {
+            const { data } = await axios.get('http://localhost:3000/forecast', {
               params: { 
                 city: termoDeBusca 
                 }     
             })
             setResultados(data)
         }
-        if(termoDeBusca)
+        if (termoDeBusca && resultados.length >= 3) {
             fazerBusca()
-    }, [termoDeBusca])
+        } else {
+            const timeoutID = setTimeout(() => {
+                if (termoDeBusca)
+                    fazerBusca()
+            }, 2000)
+            return () => {
+                clearTimeout(timeoutID)
+            }
+        }
+    }, [resultados.length, termoDeBusca])
 
   return (
     <div>
@@ -33,11 +44,28 @@ const Busca = () => {
             />
         </IconField>
         {}
-        {
-            resultados.map((resultado, index) => (
-            <Sanfona key={index} dados={resultado} />
-            ))
-        }
+        {resultados?.forecasts?.map((item, index) => (
+  <div 
+    key={index}
+    className="my-2 border border-1 border-400">
+    
+    <div className="border-bottom border-1 border-400 p-2 text-center font-bold">
+      {resultados.city}, {resultados.country}
+    </div>
+
+    <div className="p-2">
+      <p><strong>Data:</strong> {item.datetime}</p>
+      <p><strong>Temp. Mínima:</strong> {item.temp_min}°C</p>
+      <p><strong>Temp. Máxima:</strong> {item.temp_max}°C</p>
+      <p><strong>Umidade:</strong> {item.humidity}%</p>
+      <p><strong>Descrição:</strong> {item.description}</p>
+      <img 
+        src={`https://openweathermap.org/img/wn/${item.icon}@2x.png`} 
+        alt={item.description} 
+      />
+    </div>
+  </div>
+))}
 
     </div>
   )
